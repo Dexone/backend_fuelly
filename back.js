52548f6a6ee0763b9ae1dec71f77a29a5a13547d
Dexone <dm346@mail.ru>
2024-05-25 19:12:26 +0300

    update

---
+++ back.js
@@ -0,0 +1,15 @@
+import pkg from 'json-server';
+const { create, router: _router, defaults } = pkg;
+
+const server = create();
+const router = _router('./db.json');
+const middlewares = defaults();
+
+server.use(middlewares);
+server.use(router);
+
+const PORT = 1000; // Change the port if needed
+
+server.listen(PORT, () => {
+  console.log(`JSON Server is running on port ${PORT}`);
+});
